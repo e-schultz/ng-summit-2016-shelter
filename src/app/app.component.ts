edf3e762db7f87a08cd46ca6c7c2f552d359ba15
Evan Schultz <evan@rangle.io>
2016-09-13 12:16:08 -0400

    break form into own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,9 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-  let messages = this.horizonService.horizon('cats');
-  
 
-    messages.fetch().subscribe(msg => console.log(msg),
-    err=>console.log('err!'),
-    d=>console.log('Done!'));
-}
+  }
   createCat(catModel: any, isValid: boolean) {
-
     this.catsActions.createCat(catModel);
   }
 }
