a9478735d9947cc480e43f9a702a6c3dae3d7afb
Evan Schultz <evan@rangle.io>
2016-09-13 11:59:46 -0400

    starting with api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { select, NgRedux } from 'ng2-redux';
 import { Observable } from 'rxjs';
 import { ICat, IAppState, CatsActions } from './store';
-
+import { HorizonService } from './shared';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   providers: [CatsActions]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'app works!';
   @select() cats$: Observable<ICat[]>;
-  constructor(private catsActions: CatsActions) {
+  constructor(private catsActions: CatsActions, private horizonService: HorizonService) {
+
   }
 
   ngOnInit() {
-  }
+  let messages = this.horizonService.horizon('cats');
+  
+
+    messages.fetch().subscribe(msg => console.log(msg),
+    err=>console.log('err!'),
+    d=>console.log('Done!'));
+}
   createCat(catModel: any, isValid: boolean) {
-    
+
     this.catsActions.createCat(catModel);
   }
 }
