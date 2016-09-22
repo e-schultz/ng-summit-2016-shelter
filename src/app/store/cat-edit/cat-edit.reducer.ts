59b8a45ad832baec9fe14020bf7d880ccfb88b7a
Evan Schultz <evan@rangle.io>
2016-09-22 15:28:22 -0400

    Revert "Revert "Cleaning up Cats Loading""
    This reverts commit 67f34ca2950f9ebad36b6d7a3954ad23c56e3c91.

--- src/app/store/cat-edit/cat-edit.reducer.ts
+++ src/app/store/cat-edit/cat-edit.reducer.ts
@@ -1,38 +1,37 @@
-import { ICat } from '../cats';
+import { CatActions } from '../cats';
+import { ICatEdit } from './cat-edit.types';
+import { INITIAL_STATE } from './cat-edit.initial-state';
 
-interface ICatEdit  {
-  isEditing: boolean;
-  isPending: boolean;
-  currentCat: ICat;
-}
-const INITIAL_STATE: ICatEdit = {
-  isEditing: false,
-  currentCat: null,
-  isPending: false,
-};
+let setPending = (state, isPending) => Object.assign({}, state, { isPending });
 
-let setPending = (state, isPending) => {
-  return Object.assign({}, state, {isPending});
-};
-export const catEdit = (state= INITIAL_STATE, action) => {
+let { CAT_SELECTED,
+  CAT_CLEARED,
+  UPDATING_CAT,
+  CREATING_CAT,
+  CAT_CREATED,
+  CAT_UPDATED,
+  CAT_UPDATE_ERROR,
+  CAT_CREATE_ERROR } = CatActions;
+
+export const catEdit = (state:ICatEdit = INITIAL_STATE, action) => {
 
   switch (action.type) {
-    case 'CAT_SELECTED':
+    case CAT_SELECTED:
       return Object.assign({},
-      state,
-      { isEditing: true },
-      action.payload );
-    case 'CAT_CLEARED':
+        state,
+        { isEditing: true },
+        action.payload);
+    case CAT_CLEARED:
       return INITIAL_STATE;
-  case 'UPDATING_CAT':
-  case 'CREATING_CAT':
-    return setPending(state, true);
-  case 'CAT_CREATED':
-  case 'CAT_UPDATED':
-  case 'CAT_UPDATE_ERROR':
-  case 'CAT_CREATE_ERROR':
-    return setPending(state, false);
-  default:
-     return state;
+    case UPDATING_CAT:
+    case CREATING_CAT:
+      return setPending(state, true);
+    case CAT_CREATED:
+    case CAT_UPDATED:
+    case CAT_UPDATE_ERROR:
+    case CAT_CREATE_ERROR:
+      return setPending(state, false);
+    default:
+      return state;
   }
 };
