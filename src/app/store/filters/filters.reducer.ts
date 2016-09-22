2a950abbcaf6f84de1fbd541b49d76fa00ddb3b2
Evan Schultz <evan@rangle.io>
2016-09-22 09:38:44 -0400

    Rename filter predicate

--- src/app/store/filters/filters.reducer.ts
+++ src/app/store/filters/filters.reducer.ts
@@ -1,8 +1,8 @@
 import { FilterActions } from './filter.actions';
 import { combineReducers } from 'redux';
 import { getIn } from '../../shared';
-// const R = require('ramda');
-const filterReducer = (property, INITIAL_STATE = {}) => (state = INITIAL_STATE, action) => {
+
+const createFilterReducer = (property, INITIAL_STATE = {}) => (state = INITIAL_STATE, action) => {
   if (getIn(action, ['payload', 'property']) !== property && action.type !== FilterActions.CLEAR_FILTERS) {
     return state;
   }
@@ -18,66 +18,34 @@ const filterReducer = (property, INITIAL_STATE = {}) => (state = INITIAL_STATE, 
   }
 };
 
-const age = filterReducer('age');
-const breed = filterReducer('breed');
-const gender = filterReducer('gender');
-let count = 0;
-export const activeFilters = (filters) => {
-  console.log('hitting this',++count);
+const age = createFilterReducer('age');
+const breed = createFilterReducer('breed');
+const gender = createFilterReducer('gender');
+
+export const filters = combineReducers({ age, breed, gender });
+
+/**
+ * The shape of the filter state before applying this looks something like:
+ * ```
+ * {
+ *   age: {
+ *    senior: true
+ *   }
+ * }
+ * ```
+ * This function converts the state, into a function that can be used in an filter to remove
+ * items that do not match.
+ */
+export const filterCheck = (filterState) => {
+
   const getSelected = R.pipe(R.toPairs, R.filter(filterPair => filterPair[1]), R.map(filterPair => filterPair[0]));
-  const selectedBreed = getSelected(filters.breed);
-  const selectedAge = getSelected(filters.age);
-  const selectedGender = getSelected(filters.gender);
+  const selectedBreed = getSelected(filterState.breed);
+  const selectedAge = getSelected(filterState.age);
+  const selectedGender = getSelected(filterState.gender);
   const filterPredicate = (prop, filter) => (cat) => filter.length === 0 || filter.indexOf(cat[prop]) >= 0;
   const breedFilter = filterPredicate('breed', selectedBreed);
   const ageFilter = filterPredicate('age', selectedAge);
   const genderFilter = filterPredicate('gender', selectedGender);
-  return R.allPass([breedFilter,ageFilter,genderFilter]);
+  return R.allPass([breedFilter, ageFilter, genderFilter]);
 };
 
-
-/*
-const age = (state = {}, action) => {
-
-  if (getIn(action, ['payload', 'property']) !== 'age') {
-    return state;
-  }
-   switch (action.type) {
-    case FilterActions.FILTER_ADDED:
-      return Object.assign({}, state, {[action.payload.value]: true});
-  case FilterActions.FILTER_REMOVED:
-    return Object.assign({}, state, {[action.payload.value]: false});
-   default:
-   return state;
-  }
-};
-
-const breed = (state = {domesticLong:true}, action) => {
-  if (getIn(action, ['payload', 'property']) !== 'breed') {
-    return state;
-  }
-  switch (action.type) {
-    case FilterActions.FILTER_ADDED:
-      return Object.assign({}, state, {[action.payload.value]: true});
-  case FilterActions.FILTER_REMOVED:
-    return Object.assign({}, state, {[action.payload.value]: false});
-   default:
-   return state;
-  }
-};
-
-const gender = (state = {}, action) => {
-  if (getIn(action, ['payload', 'property']) === 'gender') {
-    return state;
-  }
-  switch (action.type) {
-    case FilterActions.FILTER_ADDED:
-      return Object.assign({}, state, {[action.payload.value]: true});
-  case FilterActions.FILTER_REMOVED:
-    return Object.assign({}, state, {[action.payload.value]: false});
-   default:
-   return state;
-  }
-};*/
-
-export const filters = combineReducers({age, breed, gender});
